cf5643cb12d8be980ee7fd2a153333b39b96ed0b
“mkambo1993” <manpreet.sk93@gmail.comssh-keygen -t ed25519 -C manpreet.sk93@gmail.com>
2026-08-14 22:55:38 -0400

    pushing new files

---
+++ Mannu.java
@@ -0,0 +1,16 @@
+class Mannu
+{
+	public static void main(String[] args)
+	{
+	int a=69;
+	int b=31;
+	int sum=a+b;
+	int subtract=a-b;
+	int multiply=a*b;
+	float divide=(float)a/b;
+	System.out.println("Total sum is = "+sum);
+	System.out.println("Total subtraction is = "+subtract);
+	System.out.println("Total multiple is = "+multiply);
+	System.out.println("Total division is = "+divide);
+	}
+}
